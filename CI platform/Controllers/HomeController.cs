1c0f7b0c020bf6a60a0b35a5853dbe461c8ac0e3
shyam <kanjiyashyam15@gmail.com>
2023-02-14 11:58:45 +0530

    third commit

--- CI platform/Controllers/HomeController.cs
+++ CI platform/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace CI_platform.Controllers
             return View();
         }
 
+        public IActionResult changePassword()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
