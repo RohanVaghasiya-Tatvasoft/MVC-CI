c871eea24c3cc1cecc7ab347d230d82e023978bc
shyam <kanjiyashyam15@gmail.com>
2023-02-21 16:33:47 +0530

    10th commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using CI_platform.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CI_platform.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult login()
+        {
+            return View();
+        }
+
+        public IActionResult registration()
+        {
+            return View();
+        }
+
+        public IActionResult forgotPassword()
+        {
+            return View();
+        }
+
+        public IActionResult changePassword()
+        {
+            return View();
+        }
+
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
